0799ae242ee200715031443bfbad7cb457f41dcf
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-25 19:37:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,5 +16,6 @@ class School
     @roster[grade] ||= []
     @roster[grade] << name
     #if the grade exists, do nothing. If it doesn't exist, make it and set the value to an empty array.
+    #the or/equals on line 16 serves the same function as the if statement from line 11
   end
 end
