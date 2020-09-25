182bb837e1aad09bdb8c9d47c8d7d34fa6178c11
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-25 19:39:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,14 @@
 class School
-
+  #readers for the instance variables in the initialize method
   attr_reader :name, :roster
-
+  
+  #intitializes each instance of school with a name and creates an empty roster hash for it.
   def initialize(name)
     @name = name
     @roster = {}
   end
 
+  #method for adding a new student to the roster hash
   def add_student(name, grade)
     #if @roster.has_key?(grade)
     #  @roster[grade] << name
@@ -15,7 +17,7 @@ class School
     #end
     @roster[grade] ||= []
     @roster[grade] << name
-    #if the grade exists, do nothing. If it doesn't exist, make it and set the value to an empty array.
+    #if the grade key exists, do nothing. If it doesn't exist, make it and set the value to an empty array.
     #the or/equals on line 16 serves the same function as the if statement from line 11
   end
 end
