75744effeea215328fa6b91deca457518f88750f
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-25 19:46:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -19,5 +19,10 @@ class School
     @roster[grade] << name
     #if the grade key exists, do nothing. If it doesn't exist, make it and set the value to an empty array.
     #the or/equals on line 16 serves the same function as the if statement from line 11
+    #if self.roster[grade] already exists, leave it alone! else set self.roster[grade] = []
+  end
+
+  def grade(grade)
+    @roster[grade]
   end
 end
